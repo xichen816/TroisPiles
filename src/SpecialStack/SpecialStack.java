cb90a4b75099e9c61eaafa881b66447f06ceb849
ilodule <ilonaledu@hotmail.com>
2024-02-05 10:20:32 -0500

    UPDATE de JavaDoc

--- src/SpecialStack/SpecialStack.java
+++ src/SpecialStack/SpecialStack.java
@@ -1,44 +1,51 @@
 package SpecialStack;
 
 /**
- * TODO: javadoc
- * @param <T>
+ * Une interface qui représente une pile
+ *
+ * @param <T> le type d'élément dans le stack
  */
 public interface SpecialStack<T> {
 
     /**
+     * Permet d'empiler un élément
      *
-     * @param t
+     * @param t l'élément qui est empilé
      */
     void push(T t);
 
     /**
+     * Permet de retourner et de retirer le dernier élément empilé
      *
-     * @return
+     * @return dernier élément de la pile
      */
     T pop();
 
     /**
+     * Permet de voir l'élément au sommet de la pile
      *
-     * @return
+     * @return l'élément au sommet de la pile
      */
     T top();
 
     /**
+     * Permet d'obtenir la taille de la pile
      *
-     * @return
+     * @return la taille de la pile
      */
     int size();
 
     /**
+     * Vérifie si la pile est vide
      *
-     * @return
+     * @return un boolean précisant si la pile est vide ou nan
      */
     boolean isEmpty();
 
     /**
+     * Renvoie l'élément qui possède la plus grande valeur 
      *
-     * @return
+     * @return l'élément qui possède la valeur maximale de la pile
      */
     T getMax();
 }
